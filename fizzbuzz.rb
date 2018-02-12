a2dc4f25858caf34714b8fa9942cca06369485ea
Maverick Tesini <mavericktesini@gmail.com>
2018-02-12 20:12:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz
-
+def fizzbuzz(number)
+  if ((number / 5) % 0)
+    puts "Buzz"
+  elsif ((number / 3) % 0)
+    puts "Fizz"
+  elsif ((number / 5) % 0)
+    puts "Buzz"
+  else
+    puts "nil"
 end
